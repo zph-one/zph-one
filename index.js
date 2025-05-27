68bfd87e5d6bff39501a421f8bfc2ce3b7e89a39
zph-one <17764881077@163.com>
2025-05-27 09:59:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,1 @@
+"https://c4.yonyoucloud.com/mdf-node/fragment/PND6MEF6ST5AAACList?domainKey=developplatform&busiObj=PND6MEF6ST5AAAC&terminalType=1&serviceCode=025042491d6847458aa994c171f60fe3&feV=6";
