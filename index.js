d715590c6010cdbe476a008d141d99b9b9a17a43
zph-one <17764881077@163.com>
2025-05-28 14:20:49 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-fetch("https://c4.yonyoucloud.com/mdf-node/fragment/PND6MEF6ST5AAACList?domainKey=developplatform&busiObj=PND6MEF6ST5AAAC&terminalType=1&serviceCode=025042491d6847458aa994c171f60fe3&feV=6")
-  .then(response => response.json())
-  .then(data => {
-    console.log("API返回数据:", data);
-    window.API_DATA = data; // 存储到全局变量
-  })
-  .catch(error => {
-    console.error("请求失败:", error);
-  });
